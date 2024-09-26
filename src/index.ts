2982a5040d6352d4991f08339f7266acfe87924e
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-09-26 14:27:04 -0400

    assets

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,10 @@ const config: Phaser.Types.Core.GameConfig = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 }
+      gravity: { 
+        y: 200,
+        x: 0,
+      }
     }
   },
   scene: {
