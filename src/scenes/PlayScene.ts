ee6dbecd12f9d4a16879b8365c61f7245290a4b4
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-09-27 12:04:09 -0400

    velocity y + debug

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -1,45 +1,8 @@
 import Phaser from "phaser";
 
-type Age = number
-
-let age: Age = 100
-
-declare module DinoGame {
-
-  namespace Rendering {
-    interface Person{
-      name: string;
-    }
-
-    namespace Arcade {
-      interface Whatever {
-        a: string;
-        b: number;
-      }
-    }
-
-    namespace Matter {
-      interface Whatever {
-        a: number;
-        b: string;
-      }
-    }
-  }
-}
-
-type MatterWhatever = DinoGame.Rendering.Arcade.Whatever
-interface Person{
-  name: string;
-  age: number;
-  displayAge: () => void;
-  getName: () => string;
-  welcomePerson: (welcome: string) => string;
-}
-
 class PlayScene extends Phaser.Scene {
 
-  person: Person;
-  whatever: MatterWhatever
+  player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
 
   get gameHeight() {
     return this.game.config.height as number;
@@ -51,40 +14,25 @@ class PlayScene extends Phaser.Scene {
   create() {
     this.createEnvironment();
     this.createPlayer();
+    this.registerPlayerControl();
 
-    this.whatever = {
-      a: 'kerlin',
-      b: 100,
-    }
-
-    this.person = {
-      name: 'Kerlin',
-      age: 30,
-      displayAge(){
-        console.log(this.age)
-      },
-      getName(){
-        return this.name;
-      },
-      welcomePerson(welcome: string){
-        return `${welcome} ${this.name}`
-      }
-    }
-    
-    this.person.displayAge()
-    const name = this.person.getName()
-    console.log(name)
-    this.person.welcomePerson('Hello')
   }
 
   createEnvironment(){
     this.add
       .tileSprite(0, this.gameHeight, 90, 26, 'ground')
-      .setOrigin(0, 1)
+      .setOrigin(0, 1);
   }
 
   createPlayer(){
-    this.physics.add.sprite(0, this.gameHeight, 'dino-idle').setOrigin(0, 1)
+    this.player = this.physics.add.sprite(0, this.gameHeight, 'dino-idle').setOrigin(0, 1);
+  }
+
+  registerPlayerControl(){
+    const spaceBar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    spaceBar.on('down', () => {
+      this.player.setVelocityY(-1600)
+    })
   }
 }
 
