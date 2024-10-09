5788748496c7e533876814d72f87b81c845c981c
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-10-09 10:15:03 -0400

    rollout floor

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -5,10 +5,14 @@ import { Player } from "../entities/Player";
 class PlayScene extends Phaser.Scene {
 
   player: Player;
+  ground: Phaser.GameObjects.TileSprite;
   startTrigger: SpriteWithDynamicBody;
 
   get gameHeight() {
     return this.game.config.height as number;
+  }
+  get gameWidth() {
+    return this.game.config.width as number;
   }
   constructor() {
     super('PlayScene')
@@ -22,18 +26,37 @@ class PlayScene extends Phaser.Scene {
       .setOrigin(0, 1);
 
       this.physics.add.overlap(this.startTrigger, this.player, () => {
+        if(this.startTrigger.y === 10){
+          this.startTrigger.body.reset(0, this.gameHeight)
+          return
+        }
 
+        this.startTrigger.body.reset(9999,9999)
+
+        this.time.addEvent({
+          delay: 1000/ 60,
+          loop: true,
+          callback: () => {
+            if(this.ground.width <= this.gameWidth)
+            this.ground.width += (17 * 2)
+          }
+        })
       })
   }
 
   createEnvironment() {
-    this.add
+
+    this.ground = this.add
       .tileSprite(0, this.gameHeight, 90, 26, 'ground')
       .setOrigin(0, 1);
   }
 
   createPlayer() {
     this.player = new Player(this, 0, this.gameHeight) 
+  }
+
+  update(time: number, delta: number): void {
+    // this.ground.width += 17
   }
 }
 
