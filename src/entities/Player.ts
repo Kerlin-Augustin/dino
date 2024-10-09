ff81c5ad97e10c18d2d6af5ebb46e471173ab2bd
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-10-09 09:30:52 -0400

    jumping

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -1,4 +1,7 @@
 export class Player extends Phaser.Physics.Arcade.Sprite{
+
+  cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+
   constructor(scene: Phaser.Scene, x: number, y: number){
     super(scene, x, y, 'dino-idle')
     scene.add.existing(this);
@@ -9,6 +12,9 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
   }
 
   init(){
+
+    this.cursors = this.scene.input.keyboard.createCursorKeys()
+
     this
       .setOrigin(0, 1)
       .setGravityY(5000)
@@ -17,7 +23,13 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
   }
 
   update() {
-    console.log('Player Update')
+    const { space } = this.cursors;
+    const isSpaceJustDown = Phaser.Input.Keyboard.JustDown(space)
+    const onFloor = (this.body as Phaser.Physics.Arcade.Body).onFloor()
+   
+    if(isSpaceJustDown && onFloor){
+      this.setVelocityY(-1600)
+    }
   }
 
 }
