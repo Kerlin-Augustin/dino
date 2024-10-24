8b56ce1f42f8743faaafa834fdeac20a922eea18
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-10-24 13:41:26 -0400

    score board

--- src/scenes/PlayScene.ts
+++ src/scenes/PlayScene.ts
@@ -12,6 +12,7 @@ class PlayScene extends GameScene {
   clouds: Phaser.GameObjects.Group;
   startTrigger: SpriteWithDynamicBody;
 
+  scoreText: Phaser.GameObjects.Text;
   gameOverContainer: Phaser.GameObjects.Container;
   gameOverText: Phaser.GameObjects.Image;
   restartText: Phaser.GameObjects.Image;
@@ -30,6 +31,7 @@ class PlayScene extends GameScene {
     this.createObstacles();
     this.createGameoverContainer();
     this.createAnimations();
+    this.createScore();
 
     this.handleGameStart();
     this.handleObstacleCollisions();
@@ -97,13 +99,24 @@ class PlayScene extends GameScene {
       .setAlpha(0);
   }
 
-  createAnimations(){
+  createAnimations() {
     this.anims.create({
       key: 'enemy-bird-fly',
       frames: this.anims.generateFrameNumbers('enemy-bird'),
-      frameRate: 6, 
+      frameRate: 6,
       repeat: -1
     })
+  }
+
+  createScore() {
+    this.scoreText = this.add.text(this.gameWidth, 0, '00000', {
+      fontSize: 30,
+      fontFamily: 'Arial',
+      color: '#535353',
+      resolution: 5
+    })
+      .setOrigin(1, 0)
+      .setAlpha(0)
   }
 
   spawnObstacle() {
@@ -154,6 +167,7 @@ class PlayScene extends GameScene {
             this.ground.width = this.gameWidth
             this.player.setVelocityX(0)
             this.clouds.setAlpha(1)
+            this.scoreText.setAlpha(1)
             this.isGameRunning = true;
           }
         }
