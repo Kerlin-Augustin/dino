34941464c81a9c81ae0478512649a54085087858
Kerlin Augustin <kerlin.augustin1@gmail.com>
2024-10-29 22:15:37 -0400

    hit sound

--- src/entities/Player.ts
+++ src/entities/Player.ts
@@ -5,6 +5,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
   cursors: Phaser.Types.Input.Keyboard.CursorKeys;
   scene: GameScene;
   jumpSound: Phaser.Sound.HTML5AudioSound
+  hitSound: Phaser.Sound.HTML5AudioSound
 
   constructor(scene: GameScene, x: number, y: number){
     super(scene, x, y, 'dino-run')
@@ -28,8 +29,7 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
       .setDepth(1)
 
       this.registerAnimations()
-
-      this.jumpSound = this.scene.sound.add('jump', {volume: 1}) as Phaser.Sound.HTML5AudioSound
+      this.registerSounds()
   }
 
   update() {
@@ -92,9 +92,15 @@ export class Player extends Phaser.Physics.Arcade.Sprite{
 
   }
 
+  registerSounds(){
+    this.jumpSound = this.scene.sound.add('jump', {volume: 0.5}) as Phaser.Sound.HTML5AudioSound
+    this.hitSound = this.scene.sound.add('hit', {volume: 1}) as Phaser.Sound.HTML5AudioSound
+  }
+
   die(){
     this.anims.pause();
     this.setTexture('dino-hurt');
+    this.hitSound.play();
   }
 
 }
